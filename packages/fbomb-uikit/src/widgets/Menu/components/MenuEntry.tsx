f0d90e7448b11dc83ba7da14773b9e8bc6e5f7d2
lookeey <lowkeylookee@gmail.com>
2021-08-01 16:01:30 -0300

    Change individual font sizes

--- packages/fbomb-uikit/src/widgets/Menu/components/MenuEntry.tsx
+++ packages/fbomb-uikit/src/widgets/Menu/components/MenuEntry.tsx
@@ -32,7 +32,7 @@ const MenuEntry = styled.div<Props>`
   align-items: center;
   height: ${MENU_ENTRY_HEIGHT}px;
   padding: ${({ secondary }) => (secondary ? "0 32px" : "0 16px")};
-  font-size: ${({ secondary }) => (secondary ? "14px" : "16px")};
+  font-size: ${({ secondary }) => (secondary ? "18px" : "20px")};
   font-weight: 600;
   background-color: ${({ secondary, theme }) => (secondary ? theme.colors.background : "transparent")};
   color: ${({ theme }) => theme.colors.textSubtle};
