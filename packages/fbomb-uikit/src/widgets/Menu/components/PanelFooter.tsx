7aa8be6b795551cfd2a8c5ef5a6eec72db0a52d8
lookeey <loo.kee.s.f@gmail.com>
2021-12-04 11:44:52 -0300

    tooltip changes

---
+++ packages/fbomb-uikit/src/widgets/Menu/components/PanelFooter.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { CogIcon } from "../../../components/Svg";
+import IconButton from "../../../components/Button/IconButton";
+import { MENU_ENTRY_HEIGHT } from "../config";
+import { PanelProps, PushedProps } from "../types";
+import BombPrice from "./BombPrice";
+import ThemeSwitcher from "./ThemeSwitcher";
+import SocialLinks from "./SocialLinks";
+
+interface Props extends PanelProps, PushedProps {}
+
+const Container = styled.div`
+  flex: none;
+  padding: 8px 4px;
+  background-color: ${({ theme }) => theme.nav.background};
+  border-top: solid 2px rgba(133, 133, 133, 0.1);
+`;
+
+const SettingsEntry = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: ${MENU_ENTRY_HEIGHT}px;
+  padding: 0 8px;
+`;
+
+const SocialEntry = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: ${MENU_ENTRY_HEIGHT}px;
+  padding: 0 16px;
+`;
+
+const PanelFooter: React.FC<Props> = ({ isOpen, openNav, toggleTheme, isDark, bombPriceUsd }) => {
+  if (!isOpen) {
+    return (
+      <Container>
+        <IconButton variant="text" onClick={() => openNav(true)}>
+          <CogIcon />
+        </IconButton>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <SocialEntry>
+        <BombPrice bombPriceUsd={bombPriceUsd} />
+        <SocialLinks />
+      </SocialEntry>
+      <SettingsEntry>
+        <ThemeSwitcher isDark={isDark} toggleTheme={toggleTheme} />
+      </SettingsEntry>
+    </Container>
+  );
+};
+
+export default PanelFooter;
